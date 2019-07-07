f0f9235be2e96b99ae284b2622ff38e40fc68ada
Devanshi Trivedi <devanshi.trivedi@sjsu.edu>
2019-07-07 15:46:30 -0700

    Added animations and easter egg.

--- project-3/README.md
+++ project-3/README.md
@@ -1,16 +1,11 @@
 # Project 3: Todo List
 
-Simple todo list app, using local storage to persist data.
-A list of data can be dynamically added, checked and removed with the data stored in local storage.
+- Simple todo list app, using local storage to persist data.
+- A list of data can be dynamically added, checked and removed with the data stored in local storage.
+- Added animations to remove the item from the list using callback functions.
 
 ## Links
 
 jQuery: http://jquery.com/
 
 localStorage: http://www.w3schools.com/html/html5_webstorage.asp
-
-## Challenge
-
-- Create a custom animation using `animate()`
-- Add a 4th question to the FAQ
-- Replace the text of one of the answers with an image or gif
--- project-3/index.js
+++ project-3/index.js
@@ -6,27 +6,41 @@ $(document).ready(function () {
         event.preventDefault();
         var item = $("#todo-list-item").val();
         if (item) {
-            $("#list-items").append(
-                "<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>"
-            );
-            localStorage.setItem("listItems", $("#list-items").html());
-            $("#todo-list-item").val("");
+            if (item.toLowerCase() == "dance") {
+                $("#list-items").append(
+                    "<li><input class='checkbox' type='checkbox'/><img src='assets/images/dance.png' alt='dancing' class='centerImage'><a class='remove'>x</a><hr></li>"
+                );
+                localStorage.setItem("listItems", $("#list-items").html());
+                $("#todo-list-item").val("");
+
+            } else {
+
+                $("#list-items").append(
+                    "<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>"
+                );
+                localStorage.setItem("listItems", $("#list-items").html());
+                $("#todo-list-item").val("");
+            }
         }
     });
 
     $(document).on("change", ".checkbox", function () {
         if ($(this).attr("checked")) {
             $(this).removeAttr("checked");
+            $(this).siblings().attr('src', 'assets/images/dance.png');
         } else {
             $(this).attr("checked", "checked");
+            $(this).siblings().attr('src', 'assets/images/danceChecked.png');
         }
         $(this).parent().toggleClass("completed");
         localStorage.setItem("listItems", $("#list-items").html());
     });
 
     $(document).on("click", ".remove", function () {
-        $(this).parent().remove();
-        localStorage.setItem("listItems", $("#list-items").html());
+        $(this).parent().fadeOut("slow", function () {
+            $(this).parent().remove;
+            localStorage.setItem("listItems", $("#list-items").html());
+        })
     });
 
 });
